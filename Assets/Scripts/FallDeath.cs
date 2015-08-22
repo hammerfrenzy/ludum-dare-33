edba335d914c6f0256fc92f51e5afb3c82c6d9ae
Wyatt <hammerfrenzy@gmail.com>
2015-08-22 16:31:46 -0500

    Getting Respawn to work

---
+++ Assets/Scripts/FallDeath.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallDeath : MonoBehaviour {
+
+    public Transform currentCheckpoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        StartCoroutine(MoveToCheckpoint(gameObject));
+    }
+
+    IEnumerator MoveToCheckpoint(GameObject gameObject)
+    {
+        float time = 0;
+        while (time < 0.5f)
+        {
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        gameObject.transform.position = currentCheckpoint.position;
+    }
+}
--- Assets/Scripts/player/Player.cs
+++ Assets/Scripts/player/Player.cs
@@ -13,18 +13,34 @@ public class Player : MonoBehaviour {
     Animator _animator;
     float _movespeed = 3;
 
-	// Use this for initialization
-	void Start () {
+    void Awake()
+    {
         _controller = GetComponent<CharacterController2D>();
         _animator = GetComponent<Animator>();
+        //_controller.onTriggerEnterEvent += onTriggerEnterEvent;
+        //_controller.onTriggerExitEvent += onTriggerExitEvent;
+    }
+
+	// Use this for initialization
+	void Start () {
+      
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
+        ProcessMovementInput();
+        ProcessActionInput();
 	}
 
-    void ProcessInput()
+    void ProcessActionInput()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+
+        }
+    }
+
+    void ProcessMovementInput()
     {
         float input = Input.GetAxis("Horizontal");
         float movement = input * Time.deltaTime * _movespeed;
@@ -44,5 +60,20 @@ public class Player : MonoBehaviour {
 
         float yRotation = transform.localScale.x == -1 ? 270 : 90;
         FlashlightLight.transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("T R I G G E R E D");
+    }
+
+    void onTriggerEnterEvent(Collider2D col)
+    {
+        Debug.Log("onTriggerEnterEvent: " + col.gameObject.name);
+    }
+
+    void onTriggerExitEvent(Collider2D col)
+    {
+        Debug.Log("onTriggerExitEvent: " + col.gameObject.name);
     }
 }
