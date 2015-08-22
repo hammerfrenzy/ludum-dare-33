a34f1471b5f159d6f0a13167e09d9626fd9bd249
Wyatt <hammerfrenzy@gmail.com>
2015-08-22 04:22:31 -0500

    Monster is moving, flashlight next

---
+++ Assets/Scripts/player/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(CharacterController2D))]
+[RequireComponent (typeof(Animator))]
+public class Player : MonoBehaviour {
+
+    public float gravity = -25f;
+
+
+    CharacterController2D _controller;
+    Animator _animator;
+    float _movespeed = 3;
+
+	// Use this for initialization
+	void Start () {
+        _controller = GetComponent<CharacterController2D>();
+        _animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    void ProcessInput()
+    {
+        float input = Input.GetAxis("Horizontal");
+        float movement = input * Time.deltaTime * _movespeed;
+        _controller.move(new Vector3(movement, gravity * Time.deltaTime, 0));
+
+        if (movement > 0 && transform.localScale.x < 0f)
+            FlipScale();
+        else if (movement < 0 && transform.localScale.x > 0f)
+            FlipScale();
+
+        _animator.SetFloat("speed", Mathf.Abs(input));
+    }
+
+    void FlipScale()
+    {
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+}
